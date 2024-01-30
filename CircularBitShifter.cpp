6955e746d25cf05ebf09f6a879e3456261e2bd7f
Teddy <80900011+teddyfresnes@users.noreply.github.com>
2024-01-30 23:24:11 +0100

    Add files via upload

--- CircularBitShifter.cpp
+++ CircularBitShifter.cpp
@@ -3,7 +3,15 @@
 #include <sstream>
 #include <bitset>
 #include <algorithm>
+#include <fstream>
 
+
+
+enum Format {
+    ASCII,
+    HEX,
+    BIN
+};
 
 
 
@@ -14,21 +22,94 @@ std::string circularShift(const std::string& binaryValue, int blockSize, int shi
     std::string result;
     for (size_t i = 0; i < binaryValue.length(); i+=blockSize)
 	{
-        std::bitset<32> block(binaryValue.substr(i, blockSize));
+        std::bitset<4096> block(binaryValue.substr(i, blockSize));
         block = (block<<shift) | (block>>(blockSize-shift));
-        result += block.to_string().substr(32-blockSize);
+        result += block.to_string().substr(4096-blockSize);
     }
+	result.erase(result.find_last_not_of('0') + 1); // efface les 0 en trop a la fin
     return result;
 }
 
 
 
+ // compte le nb de caracteres pour avoir une approximation
+int countAlphanumeric(const std::string& asciiValue)
+{
+    int count = 0;
+    for (char c : asciiValue)
+        {if (std::isalnum(c))
+			{count++;}}
+    return count;
+}
 
+
+
+
+// fonction de conversion
+std::string stringToHex(const std::string& input) {
+    std::stringstream hexStream;
+    for (unsigned char c : input) {
+        hexStream << std::hex << std::setw(2) << std::setfill('0') << static_cast<int>(c);
+    }
+    return hexStream.str();
+}
+std::string hexToString(const std::string& hexInput) {
+    if (hexInput.length() % 2 != 0) {
+        throw std::invalid_argument("La longueur de la chaîne hexadécimale doit être paire.");
+    }
+    std::stringstream asciiStream;
+    for (size_t i = 0; i < hexInput.length(); i += 2) {
+        std::string byteStr = hexInput.substr(i, 2);
+        char byte = static_cast<char>(std::stoi(byteStr, nullptr, 16));
+        asciiStream << byte;
+    }
+    return asciiStream.str();
+}
+std::string binaryToHex(const std::string& binaryValue) {
+    // check si on a bien des octets
+    if (binaryValue.length() % 8 != 0) {
+        throw std::invalid_argument("La longueur de l'argument binaire doit etre un multiple de 8");
+    }
+    std::bitset<8> bits;
+    std::stringstream hexStream;
+    for (size_t i = 0; i < binaryValue.length(); i += 8) {
+        bits = std::bitset<8>(binaryValue.substr(i, 8));
+        hexStream << std::hex << bits.to_ulong();
+    }
+    return hexStream.str();
+}
+std::string hexToBinary(const std::string& hexValue) {
+    std::stringstream binaryStream;
+    for (char c : hexValue) {
+        std::string hexChar(1, c);
+        int intValue;
+        std::istringstream(hexChar) >> std::hex >> intValue;
+        binaryStream << std::bitset<4>(intValue).to_string();
+    }
+    return binaryStream.str();
+}
+
+
+
+/* MAIN */
 int main(int argc, char* argv[])
 {
+	Format outputFormat = ASCII;
+	
+	
     if (argc < 2)
 	{
-        std::cerr<<"Utilisation: "<<argv[0]<<" <valeur_hex> [-b] [-h] [-d]"<<std::endl;
+		std::cerr << "Utilisation: " << argv[0] << " <valeur> [--format=<ascii|hex|bin>] [-b] [-h] [-d] [-i] [-e] [-m1 | -m2 | -m3]" << std::endl;
+		std::cerr << "Options :" << std::endl;
+		std::cerr << "  --format=<ascii|hex|bin> : Format de de la valeur d'entree (par defaut : ascii)" << std::endl;
+		std::cerr << "  -b : Affiche la valeur en binaire" << std::endl;
+		std::cerr << "  -h : Affiche la valeur en hexadecimal" << std::endl;
+		std::cerr << "  -d : Affiche la valeur en decimal" << std::endl;
+		std::cerr << "  -i : Affiche le compte alphanumerique de la valeur" << std::endl;
+		std::cerr << "  -e : Exporte les résultats dans un fichier 'output.txt'" << std::endl;
+		std::cerr << "  -m1 : Affiche jusqu'a 32 blocs lors du decalage circulaire (par defaut)" << std::endl;
+		std::cerr << "  -m2 : Affiche jusqu'a 512 blocs lors du decalage circulaire" << std::endl;
+		std::cerr << "  -m3 : Affiche jusqu'a 4096 blocs lors du decalage circulaire (utile pour grandes valeurs)" << std::endl;
         return 1;
     }
 
@@ -36,9 +117,15 @@ int main(int argc, char* argv[])
     bool displayBinary = false;
     bool displayHex = false;
     bool displayDecimal = false;
+	bool exportToFile = false;
+	std::ofstream outputFile;
+	bool displayAlphanumericCount = false;
+	int maxAlphanumericCount = 0;
+	int maxBlockSize = 32;
 
 
-    // check arguments
+
+    /* GESTION DES ARGUMENTS */
     for (int i=2; i<argc; ++i)
 	{
         if (std::string(argv[i]) == "-b")
@@ -53,25 +140,90 @@ int main(int argc, char* argv[])
 		{
             displayDecimal = true;
         }
+        else if (std::string(argv[i]) == "-i")
+        {
+            displayAlphanumericCount = true;
+        }
+		else if (std::string(argv[i]) == "-e")
+		{
+			exportToFile = true;
+			outputFile.open("output.txt", std::ios::out); // writing
+			if (!outputFile.is_open())
+			{
+				std::cerr << "Erreur lors de l'ouverture du fichier output.txt" << std::endl;
+				return 1;
+			}
+		}
+		else if (std::string(argv[i]) == "-m1")
+		{
+			maxBlockSize = 32;
+		}
+		else if (std::string(argv[i]) == "-m2")
+		{
+			maxBlockSize = 512;
+		}
+		else if (std::string(argv[i]) == "-m3")
+		{
+			maxBlockSize = 4096;
+		}
+		
+		else if (std::string(argv[i]) == "--format=ascii")
+		{
+			outputFormat = ASCII;
+		}
+		else if (std::string(argv[i]) == "--format=hex")
+		{
+			outputFormat = HEX;
+		}
+		else if (std::string(argv[i]) == "--format=bin")
+		{
+			outputFormat = BIN;
+		}
+        else if (std::string(argv[i]).find("--format=") == 0)
+        {
+            // prend le texte après "--format="
+            std::string formatOption = std::string(argv[i]).substr(9);
+            if (formatOption == "ascii")
+            {
+                outputFormat = ASCII;
+            }
+            else if (formatOption == "hex")
+            {
+                outputFormat = HEX;
+            }
+            else if (formatOption == "bin")
+            {
+                outputFormat = BIN;
+            }
+            else
+            {
+                std::cerr << "Format non valide. Utilisez --format=ascii, --format=hex ou --format=bin." << std::endl;
+                return 1;
+            }
+        }
     }
-
-
-    // recuperation valeur en argument
-    std::string hexValueStr(argv[1]);
-    std::stringstream ss;
-    ss << std::hex << hexValueStr;
-    unsigned int hexValue;
-    ss >> hexValue;
-
-
-    // conversion binaire
-	std::string binaryValue;
-    for (size_t i=0; i<hexValueStr.length(); i+=2)
-	{
-        std::string byteStr = hexValueStr.substr(i, 2);
-        char byte = static_cast<char>(std::stoi(byteStr, nullptr, 16));
-        binaryValue += std::bitset<8>(byte).to_string();
+	
+	
+	
+	
+	// recuperation valeur en argument
+	std::string valeur(argv[1]);
+	std::string hexValueStr; // la valeur universel est hexadecimal
+	switch (outputFormat)
+    {
+        case ASCII:
+            hexValueStr = stringToHex(valeur);
+			break;
+        case HEX:
+            hexValueStr = valeur;
+			break;
+        case BIN:
+			hexValueStr = binaryToHex(valeur);
+			break;
+        default:
+            hexValueStr = stringToHex(valeur);
     }
+	std::string binaryValue = hexToBinary(hexValueStr);
 
 
     // affichage menu
@@ -94,18 +246,20 @@ int main(int argc, char* argv[])
 	std::cout << "\nBy teddyfresnes\n\n" << std::endl;
     std::cout << "Bienvenue dans le script Circular Bit Shifter" << std::endl;
     std::cout << "Value hex : " << hexValueStr << std::endl;
-	std::cout << "Value ascii : " << static_cast<char>(hexValue) << std::endl;
+	std::cout << "Value ascii : " << hexToString(hexValueStr) << std::endl;
     std::cout << "Value binary : " << binaryValue << std::endl;
 	std::cout << "------------------------------------------------------------------------------------\n" << std::endl;
+	if (exportToFile)
+		{std::cout << "\nExport option active, resultat dans output.txt\n\n" << std::endl;}
+				
 
-
-    // operation de decalage circulaire sur tout les blocs et tout les shifts possibles (Bruteforce 2≤B≤32, 1≤N<B)
-    for (int blockSize=2; blockSize<=32; ++blockSize)
+    // operation de decalage circulaire sur tout les blocs et tout les shifts possibles (Bruteforce 2≤B≤256, 1≤N<B)
+    for (int blockSize=2; blockSize<=maxBlockSize; ++blockSize)
 	{
 		// si la taille du blocs depasse le nb de bits, on arrete
 		if (blockSize > binaryValue.length())
 		{
-			std::cerr << "La taille du bloc binaire est supérieur à la taille de la valeur binaire !" << std::endl;
+			std::cerr << "La taille du bloc binaire est superieur a la taille de la valeur binaire !" << std::endl;
 			break;
 		}
 		
@@ -129,26 +283,70 @@ int main(int argc, char* argv[])
 			std::replace(shiftedAsciiValue.begin(), shiftedAsciiValue.end(), '\t', ' ');
 			std::replace(shiftedAsciiValue.begin(), shiftedAsciiValue.end(), '\f', ' ');
 
-            // affichage ASCII
-            std::cout << "[" << blockSize << "]+ " << shift << " :	" << shiftedAsciiValue;
+            /* AFFICHAGE RESULTAT OCCURENCE */
+			if (exportToFile)
+			{
 
-            
-            if (displayHex) // affichage hex avec -h
+				// affichage ASCII
+				outputFile << "[" << blockSize << "]+ " << shift << " :    " << shiftedAsciiValue;
+
+				if (displayHex) // affichage hex avec -h
+				{
+					outputFile << "    (" << stringToHex(shiftedAsciiValue) << ")";
+				}
+				if (displayDecimal) // affichage décimal avec -d
+				{
+					outputFile << "    (" << static_cast<int>(shiftedAsciiValue[0]) << ")";
+				}
+				if (displayBinary) // affichage binaire avec -b
+				{
+					outputFile << shiftedBinaryValue;
+				}
+                if (displayAlphanumericCount) // affichage nb alphanumeric avec -i
+                {
+                    int alphanumericCount = countAlphanumeric(shiftedAsciiValue);
+					outputFile << "    {" << alphanumericCount << " alphanumeric}";
+                    maxAlphanumericCount = std::max(maxAlphanumericCount, alphanumericCount);
+
+                }
+				outputFile << std::endl;
+			}
+			else
 			{
-                std::cout << "    (" << hexValueStr << ")";
-            }
-            if (displayDecimal) // affichage décimal avec -d
-			{
-                std::cout << "    (" << static_cast<int>(shiftedAsciiValue[0]) << ")";
-            }
-            if (displayBinary) // affichage binaire avec -b
-			{
-                std::cout << std::endl << "" << shiftedBinaryValue;
-            }
-			std::cout << std::endl;
+				// affichage ASCII
+				std::cout << "[" << blockSize << "]+ " << shift << " :    " << shiftedAsciiValue;
+
+				if (displayHex) // affichage hex avec -h
+				{
+					std::cout << "    (" << stringToHex(shiftedAsciiValue) << ")";
+				}
+				if (displayDecimal) // affichage décimal avec -d
+				{
+					std::cout << "    (" << static_cast<int>(shiftedAsciiValue[0]) << ")";
+				}
+				if (displayBinary) // affichage binaire avec -b
+				{
+					std::cout << shiftedBinaryValue;
+				}
+                if (displayAlphanumericCount) // affichage nb alphanumeric avec -i
+                {
+                    int alphanumericCount = countAlphanumeric(shiftedAsciiValue);
+                    std::cout << "    {" << alphanumericCount << " alphanumeric}";
+                    maxAlphanumericCount = std::max(maxAlphanumericCount, alphanumericCount);
+                }
+				std::cout << std::endl;
+			}
         }
     }
 	
+    if (displayAlphanumericCount)
+    {
+        std::cout << "Maximum de char alphanumerique sur une occurence: " << maxAlphanumericCount << std::endl;
+    }
+	if (exportToFile)
+	{
+		outputFile.close();
+	}
 	std::cout << "END !" << std::endl;
 	
     return 0;
